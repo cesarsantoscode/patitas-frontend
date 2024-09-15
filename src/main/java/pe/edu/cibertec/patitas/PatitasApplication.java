7e9886ba3ee76ea5464fc9e0a84920541b03660f
César Santos <cesarsantostorres@gmail.com>
2024-09-15 00:17:27 -0500

    Carga inicial del proyecto

---
+++ src/main/java/pe/edu/cibertec/patitas/PatitasApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.patitas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatitasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatitasApplication.class, args);
+	}
+
+}
