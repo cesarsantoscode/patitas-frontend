da8007abde58e1eb2846fd851164a0198d6940eb
César Santos <cesarsantostorres@gmail.com>
2024-09-28 09:24:21 -0500

    COnfiguración de timeout

--- src/main/java/pe/edu/cibertec/patitas/controller/LoginController.java
+++ src/main/java/pe/edu/cibertec/patitas/controller/LoginController.java
@@ -1,16 +1,23 @@
 package pe.edu.cibertec.patitas.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
+import pe.edu.cibertec.patitas.dto.LoginRequestDTO;
+import pe.edu.cibertec.patitas.dto.LoginResponseDTO;
 import pe.edu.cibertec.patitas.viewmodel.LoginModel;
 
 @Controller
 @RequestMapping("/login")
 public class LoginController {
+
+    @Autowired
+    RestTemplate restTemplate;
 
     @GetMapping("/inicio")
     public String login(Model model) {
@@ -36,8 +43,36 @@ public class LoginController {
 
         }
 
-        LoginModel loginModel = new LoginModel("00", "", "César Santos");
-        model.addAttribute("loginModel", loginModel);
+        try {
+
+            // Invocar servicio de autenticación
+            String endpoint = "http://localhost:8081/autenticacion/login";
+            LoginRequestDTO loginRequestDTO = new LoginRequestDTO(tipoDocumento, numeroDocumento, password);
+            LoginResponseDTO loginResponseDTO = restTemplate.postForObject(endpoint, loginRequestDTO, LoginResponseDTO.class);
+
+            // Validar respuesta de servicio
+            if (loginResponseDTO.codigo().equals("00")){
+
+                LoginModel loginModel = new LoginModel("00", "", loginResponseDTO.nombreUsuario());
+                model.addAttribute("loginModel", loginModel);
+
+            } else {
+
+                LoginModel loginModel = new LoginModel("02", "Error: Autenticación fallida", "");
+                model.addAttribute("loginModel", loginModel);
+                return "inicio";
+
+            }
+
+        } catch (Exception e) {
+
+            LoginModel loginModel = new LoginModel("99", "Error: Ocurrió un problema en la autenticación", "");
+            model.addAttribute("loginModel", loginModel);
+            System.out.println(e.getMessage());
+            return "inicio";
+
+        }
+
         return "principal";
 
     }
---
+++ src/main/java/pe/edu/cibertec/patitas/dto/LoginResponseDTO.java
@@ -0,0 +1,4 @@
+package pe.edu.cibertec.patitas.dto;
+
+public record LoginResponseDTO(String codigo, String mensaje, String nombreUsuario, String correoUsuario) {
+}
