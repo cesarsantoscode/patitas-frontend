1307ae48e303cf0191a424711a7ef5d0049237a9
César Santos <cesarsantostorres@gmail.com>
2024-09-29 12:36:45 -0500

    Ajustes generales para no usar capa service

--- src/main/java/pe/edu/cibertec/patitas/controller/LoginController.java
+++ src/main/java/pe/edu/cibertec/patitas/controller/LoginController.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
 import pe.edu.cibertec.patitas.dto.LoginRequestDTO;
 import pe.edu.cibertec.patitas.dto.LoginResponseDTO;
-import pe.edu.cibertec.patitas.service.LoginService;
 import pe.edu.cibertec.patitas.viewmodel.LoginModel;
 
 @Controller
@@ -17,7 +17,7 @@ import pe.edu.cibertec.patitas.viewmodel.LoginModel;
 public class LoginController {
 
     @Autowired
-    LoginService loginService;
+    RestTemplate restTemplate;
 
     @GetMapping("/inicio")
     public String login(Model model) {
@@ -32,11 +32,10 @@ public class LoginController {
                              @RequestParam("password") String password,
                              Model model) {
 
-        // preparar request
-        LoginRequestDTO loginRequestDTO = new LoginRequestDTO(tipoDocumento, numeroDocumento, password);
-
         // validar campos de entrada
-        if (!loginService.validarRequest(loginRequestDTO)) {
+        if (tipoDocumento == null || tipoDocumento.trim().length() == 0 ||
+                numeroDocumento == null || numeroDocumento.trim().length() == 0 ||
+                password == null || password.trim().length() == 0) {
 
             LoginModel loginModel = new LoginModel("01", "Error: Debe completar correctamente sus credenciales", "");
             model.addAttribute("loginModel", loginModel);
@@ -44,18 +43,18 @@ public class LoginController {
 
         }
 
-        // consumir servicio de autenticacion
         try {
 
-            // invocar servicio de autenticación
-            LoginResponseDTO loginResponseDTO = loginService.autenticar(loginRequestDTO);
+            // Invocar servicio de autenticación
+            String endpoint = "http://localhost:8081/autenticacion/login";
+            LoginRequestDTO loginRequestDTO = new LoginRequestDTO(tipoDocumento, numeroDocumento, password);
+            LoginResponseDTO loginResponseDTO = restTemplate.postForObject(endpoint, loginRequestDTO, LoginResponseDTO.class);
 
-            // validar respuesta de servicio
+            // Validar respuesta de servicio
             if (loginResponseDTO.codigo().equals("00")){
 
                 LoginModel loginModel = new LoginModel("00", "", loginResponseDTO.nombreUsuario());
                 model.addAttribute("loginModel", loginModel);
-                return "principal";
 
             } else {
 
@@ -73,6 +72,8 @@ public class LoginController {
             return "inicio";
 
         }
+
+        return "principal";
 
     }
 
